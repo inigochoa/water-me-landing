89668c09015b9b2aa02a1a337d438e6cd0cfa44b
Iñigo Ochoa <contact@inigochoa.me>
2023-08-23 21:57:38 +0200

    feat: desktop version v1

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,28 @@ module.exports = {
     extend: {
       colors: {
         'primary': '#36483d',
-        'secondary': '#314447',
+        'secondary': '#f5b82e',
       },
       fontFamily: {
         'sans': ['Roboto', 'sans-serif'],
+      },
+      fontSize: {
+        '4.25xl': ['2.5rem', '1.2'],
+        '4.5xl': ['2.875rem', '1.07'],
+      },
+      maxWidth: {
+        'content': 'calc(1124px + 15%)',
+      },
+      minHeight: {
+        '170': '42.5rem'
+      },
+      spacing: {
+        '17': '4.25rem',
+        '25': '6.25rem',
+        '26': '6.5rem',
+        '30': '7.5rem',
+        '34': '8.5rem',
+        'heading': '7.5%',
       },
     },
   },
