75001122944c62465a1b7c06b11dcd987d1ae5cb
Iñigo Ochoa <contact@inigochoa.me>
2023-08-24 17:57:52 +0200

    feat: update to be responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,7 @@ module.exports = {
         'sans': ['Roboto', 'sans-serif'],
       },
       fontSize: {
+        '3.5xl': ['2rem', '1.2'],
         '4.25xl': ['2.5rem', '1.2'],
         '4.5xl': ['2.875rem', '1.07'],
       },
@@ -20,16 +21,22 @@ module.exports = {
         'content': 'calc(1124px + 15%)',
       },
       minHeight: {
-        '170': '42.5rem'
+        '128': '32rem',
+        '140': '35rem',
       },
       spacing: {
+        '15': '3.75rem',
         '17': '4.25rem',
         '25': '6.25rem',
         '26': '6.5rem',
         '30': '7.5rem',
-        '34': '8.5rem',
         'heading': '7.5%',
       },
+    },
+    screens: {
+      'tablet': '768px',
+      'laptop': '1024px',
+      'desktop': '1280px',
     },
   },
   plugins: [
