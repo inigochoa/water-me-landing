30d64c61eb7fc118d72242f4bf2a3f6d5b52e538
inigochoa <inigochoa@gmail.com>
2023-08-24 18:10:01 +0000

    Deploying to gh-pages from @ inigochoa/water-me-landing@3bea44bf2595d276ef2ca0d6e00577fbfa249d24 🚀

---
+++ index.27f9e60c.js
@@ -0,0 +1,2 @@
+document.getElementById("btn-call-to-action").addEventListener("click",()=>{let e=document.getElementById("call-to-action").offsetTop;window.scroll({behavior:"smooth",left:0,top:e})}),window.onscroll=()=>{let e=document.getElementById("header");0===window.scrollY?e.classList.remove("shadow-md"):e.classList.add("shadow-md")};
+//# sourceMappingURL=index.27f9e60c.js.map
